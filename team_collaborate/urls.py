eaed23897dddbe6b690204b22744a44e3a39c698
Rhythmvashist <vashistrhythm1@gmail.com>
2019-11-23 20:09:08 -0800

    few changes

--- team_collaborate/urls.py
+++ team_collaborate/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# Admin-Admin
+
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url, include
+from django.urls import path, include
+from django.conf.urls import url
 from rest_framework import routers, serializers, viewsets
 from django.contrib.auth.models import User
 
@@ -41,5 +43,6 @@ router.register(r'users', UserViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('admin/', admin.site.urls),
 ]
